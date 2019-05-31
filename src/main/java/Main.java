5926dc6a1540e08be51f53465f73b9f024ec0a6b
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-05-31 13:38:17 -0600

    Can compile on osx....... need to figure out why profile activation isn't working

---
+++ src/main/java/Main.java
@@ -0,0 +1,80 @@
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+/**
+ * @author Paul Nelson Baker
+ * @author WGU Student Info: pbake22 - #000412290
+ * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
+ * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
+ * @since 2019-05
+ */
+public class Main implements AutoCloseable {
+
+    private long windowHandle;
+
+    public static void main(String... args) {
+        try (Main main = new Main()) {
+            main.run();
+        }
+    }
+
+    public void run() {
+        System.out.println("Starting LWJGL " + Version.getVersion());
+        init();
+        loop();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        windowHandle = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
+        if (windowHandle == NULL) {
+            throw new RuntimeException("Failed to create the GLFW window");
+        }
+        glfwSetKeyCallback(windowHandle, (windowHandle, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                glfwSetWindowShouldClose(windowHandle, true);
+            }
+        });
+        try (MemoryStack stack = stackPush()) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+            glfwGetWindowSize(windowHandle, pWidth, pHeight);
+            GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+            glfwSetWindowPos(
+                    windowHandle,
+                    (vidMode.width() - pWidth.get(0)) / 2,
+                    (vidMode.height() - pHeight.get(0)) / 2
+            );
+        }
+        glfwMakeContextCurrent(windowHandle);
+        glfwSwapInterval(1);
+        glfwShowWindow(windowHandle);
+    }
+
+    private void loop() {
+
+    }
+
+    @Override
+    public void close() {
+        glfwFreeCallbacks(windowHandle);
+        glfwDestroyWindow(windowHandle);
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+}
